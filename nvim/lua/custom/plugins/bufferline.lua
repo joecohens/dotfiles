b11359586e4087ad67af370307e410cbe15f3846
Joe Cohen <joecohens@users.noreply.github.com>
2024-03-06 18:18:01 -0600

    updates

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -2,9 +2,21 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    vim.opt.termguicolors = true
-
-    require("bufferline").setup()
-  end,
+  opts = {
+    options = {
+      mode = "buffers",
+      max_name_length = 26,
+      diagnostics = "nvim_lsp",
+      diagnostics_update_in_insert = true,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "File Explorer",
+          highlight = "Directory",
+          text_align = "left",
+          separators = true,
+        },
+      },
+    },
+  },
 }
