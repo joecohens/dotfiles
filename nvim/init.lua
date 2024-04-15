58f1040b47e022ff3a8bca107315b6ecce5dd703
Joe Cohen <joecohens@users.noreply.github.com>
2024-04-15 13:20:26 -0600

    add hidden files and lsps

--- nvim/init.lua
+++ nvim/init.lua
@@ -534,7 +534,8 @@ require("lazy").setup({
         -- clangd = {},
         gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+        intelephense = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
--- nvim/lua/custom/plugins/neo_tree.lua
+++ nvim/lua/custom/plugins/neo_tree.lua
@@ -11,6 +11,11 @@ return {
       window = {
         position = "right",
       },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
     })
 
     vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle<CR>")
