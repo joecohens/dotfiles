516d7363a513c2faa2865125066f94a08c8d8289
Joe Cohen <joecohens@users.noreply.github.com>
2024-04-16 20:43:16 -0600

    use nvchad

---
+++ nvim/lua/configs/conform.lua
@@ -0,0 +1,20 @@
+local options = {
+  formatters_by_ft = {
+    lua = { "stylua" },
+    go = { "goimports", "gofmt" },
+    javascript = { { "prettierd", "prettier" } },
+    php = { "intelephense" },
+    rust = { "rustfmt" },
+
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+require("conform").setup(options)
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "typescript-language-server",
+        "lua-language-server",
+        "rust-analyzer",
+        "intelephense",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "gopls",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "php",
+        "go",
+        "tsx",
+        "typescript",
+        "javascript",
+      },
+    },
+  },
+
+  -- custom plugins
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup()
+    end,
+  },
+
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
