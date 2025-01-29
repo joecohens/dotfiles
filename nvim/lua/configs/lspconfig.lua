147606c6e7388b62a391c08ab07e5bf6fc229e50
Joe Cohen <joecohens@users.noreply.github.com>
2025-01-28 20:17:34 -0600

    updates

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 local servers = {
-  "tsserver",
+  "ts_ls",
   "html",
   "gopls",
   "intelephense",
@@ -22,7 +22,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
