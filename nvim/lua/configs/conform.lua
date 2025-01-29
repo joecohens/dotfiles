dca73b4b30a9869330b4d81a2b05f07422516d3b
Joe Cohen <joecohens@users.noreply.github.com>
2025-01-29 10:41:33 -0600

    update nvchad

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -17,4 +17,4 @@ local options = {
   },
 }
 
-require("conform").setup(options)
+return options
--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,9 @@
--- EXAMPLE
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
+
+-- EXAMPLE
 local servers = {
   "ts_ls",
   "html",
@@ -11,19 +11,20 @@ local servers = {
   "intelephense",
   "rust_analyzer",
 }
+local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
   }
 end
 
--- typescript
+-- configuring single server, example: typescript
 lspconfig.ts_ls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
 }
